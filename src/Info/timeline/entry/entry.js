46f8d0709dc46640f5139e7f1ebbe6e7c3218b9c
Maxim Fishman <maxim.fishman@kcl.ac.uk>
2020-06-03 18:51:23 -0700

    dynamically-determined timeline height

--- src/Info/timeline/entry/entry.js
+++ src/Info/timeline/entry/entry.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './entry.css';
 
-const Entry = () => {
+const Entry = ({ yearInterval, title, description }) => {
     return (
         <div className={'entry'}>
-            <div className={'entryYear'}>2018 -PRESENT</div>
+            <div className={'entryYear'}>{yearInterval}</div>
             <div className={'entryDot'}/>
             <div className={'entryText'}>
-                <div className={'entryTitle'}>title</div>
-                <div className={'entryDescription'}>description</div>
+                <div className={'entryTitle'}>{title}</div>
+                <div className={'entryDescription'}>{description}</div>
             </div>
         </div>
     );
 };
 
 export default Entry;
+
+Entry.propTypes = {
+  yearInterval: PropTypes.string,
+  title: PropTypes.string,
+  description: PropTypes.string
+};
--- src/Info/timeline/timeline.js
+++ src/Info/timeline/timeline.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import './timeline.css';
 import Entry from "./entry/entry";
 import Subtitle from "./subtitle/subtitle";
 
 const Timeline = () => {
+    const contentRef = useRef(null);
+    const timelineRef = useRef(null);
+
+    useEffect(() => {
+        console.log(contentRef.current.clientHeight);
+        timelineRef.current.style.height = contentRef.current.clientHeight + 50 + 'px';
+    }, []);
+
     return (
-        <div className={'timeline'}>
+        <div ref={timelineRef} className={'timeline'}>
             <div className={'line'}/>
             <div className={'initialDot'}/>
-            <div className={'content'}>
+            <div ref={contentRef} className={'content'}>
+                <Subtitle text={'Me'}/>
+                <text className={'aboutMe'}>Second-year college student with a passion for Java GUI as well as web and cross-platform/native
+                    mobile app development.
+                </text>
                 <Subtitle text={'Education'}/>
-                <Entry/>
+                <Entry yearInterval={'2018 -PRESENT'} title={'King\'s College London'} description={'Computer Science Msci'}/>
+                <Entry yearInterval={'2014 -2018'} title={'Henry M. Gunn High School'}/>
             </div>
         </div>
     )
