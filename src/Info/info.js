f9faaf583a7fba97be2a1cb40a9f0c2d2110161f
Maxim Fishman <maxim.fishman@kcl.ac.uk>
2020-06-08 16:48:50 -0700

    add pie chart, name section now sticky

--- src/Info/info.js
+++ src/Info/info.js
@@ -1,13 +1,72 @@
 import React from 'react';
-import './info.css';
 import Timeline from "./timeline/timeline";
+import Section from "./timeline/section/section";
+import Subtitle from "./timeline/section/subtitle/subtitle";
+import Entry from "./timeline/section/entry/entry";
+import {PieChart} from 'react-minimal-pie-chart';
+import './info.css';
+
+const data = [
+        {title: 'Java (24001)', value: 24001, color: '#b07219'},
+        {title: 'Javascript (72297)', value: 72297, color: '#f1e05a'},
+        {title: 'Dart (5914)', value: 5914, color: '#00b4ab'},
+        {title: 'Python (501)', value: 501, color: '#3572a5'},
+        {title: 'C++ (3263)', value: 3263, color: '#f34b7d'},
+        {title: 'Scala (3066)', value: 3066, color: '#c22d40'}
+    ];
 
 const Info = () => {
-  return(
-      <div className={'infoContainer'}>
-          <Timeline/>
-      </div>
-  )
+    return (
+        <div className={'infoContainer'}>
+            <Timeline>
+                <Section>
+                    <Subtitle text={'Me'}/>
+                    <div className={'aboutMe'}>Second-year college student with a passion for Java GUI as well as web
+                        and cross-platform/native
+                        mobile app development.
+                    </div>
+                </Section>
+                <Section>
+                    <Subtitle text={'Education'}/>
+                    <Entry yearInterval={'2018 -PRESENT'} title={'King\'s College London'}
+                           description={'Computer Science Msci'}/>
+                    <Entry yearInterval={'2014 -2018'} title={'Henry M. Gunn High School'}/>
+                </Section>
+                <Section>
+                    <Subtitle text={'Experience'}/>
+                    <Entry yearInterval={'2019'} title={'Android App Development Intern'} description={''}/>
+                    <Entry yearInterval={'2018'} title={'Programmer'} description={''}/>
+                </Section>
+            </Timeline>
+            <Timeline>
+                <Section>
+                    <Subtitle text={'Total Line Changes'}/>
+                    <PieChart
+                        style={{height: 'fit-content', width: '330', marginLeft: '20px'}}
+                        data={data}
+                        label={({dataEntry}) => dataEntry.title}
+                        labelStyle={(index) => ({
+                            fill: data[index].color,
+                            fontSize: '7px',
+                            fontFamily: 'Nunito, sans-serif',
+                            fontWeight: '300',
+                            wordBreak: 'break-word',
+                            wordWrap: 'break-word',
+                            width: '2px',
+                            borderStyle: 'dashed'
+                        })}
+                        labelPosition={110}
+                        lineWidth={15}
+                        radius={45}
+                        paddingAngle={5}
+                        center={PieChart.defaultProps.center}
+                        animationDuration={PieChart.defaultProps.animationDuration}
+                        animationEasing={PieChart.defaultProps.animationEasing}
+                    />
+                </Section>
+            </Timeline>
+        </div>
+    )
 };
 
 export default Info;
--- src/Info/timeline/timeline.js
+++ src/Info/timeline/timeline.js
@@ -1,16 +1,14 @@
 import React, {useEffect, useRef} from 'react';
+import PropTypes from 'prop-types';
 import './timeline.css';
-import Entry from "./section/entry/entry";
-import Subtitle from "./section/subtitle/subtitle";
-import Section from "./section/section";
 
-const Timeline = () => {
+const Timeline = ({ children }) => {
     const contentRef = useRef(null);
     const timelineRef = useRef(null);
 
     useEffect(() => {
-        console.log(contentRef.current.clientHeight);
         timelineRef.current.style.height = contentRef.current.clientHeight + 50 + 'px';
+        timelineRef.current.style.width = contentRef.current.clientWidth + 'px';
     }, []);
 
     return (
@@ -18,27 +16,14 @@ const Timeline = () => {
             <div className={'line'}/>
             <div className={'initialDot'}/>
             <div ref={contentRef} className={'content'}>
-                <Section>
-                    <Subtitle text={'Me'}/>
-                    <div className={'aboutMe'}>Second-year college student with a passion for Java GUI as well as web
-                        and cross-platform/native
-                        mobile app development.
-                    </div>
-                </Section>
-                <Section>
-                    <Subtitle text={'Education'}/>
-                    <Entry yearInterval={'2018 -PRESENT'} title={'King\'s College London'}
-                           description={'Computer Science Msci'}/>
-                    <Entry yearInterval={'2014 -2018'} title={'Henry M. Gunn High School'}/>
-                </Section>
-                <Section>
-                    <Subtitle text={'Experience'}/>
-                    <Entry yearInterval={'2019'} title={'Android App Development Intern'} description={''}/>
-                    <Entry yearInterval={'2018'} title={'Programmer'} description={''}/>
-                </Section>
+                {children}
             </div>
         </div>
     )
 };
 
 export default Timeline;
+
+Timeline.propTypes = {
+  children: PropTypes.array
+};
