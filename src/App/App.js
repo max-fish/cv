43905d531c8bf4076e699f5721225d2e29a54628
Maxim Fishman <maxim.fishman@kcl.ac.uk>
2020-06-12 20:11:14 -0700

    prepare dimensions for pdf export

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, {Fragment, useRef} from 'react';
 import './App.css';
 import Welcome from "../Welcome/welcome";
 import Info from "../Info/info";
+import PDFExport from "@progress/kendo-react-pdf/dist/npm/PDFExport";
+
 
 function App() {
-  return (
-    <div className="App">
-      <Welcome/>
-      <Info/>
-    </div>
-  );
+    const pdfExport = useRef(null);
+
+    return (
+        <Fragment>
+            <PDFExport
+                scale={0.75}
+                paperSize="A4"
+                margin="0cm"
+                fileName="CV-Maxim Fishman"
+                ref={pdfExport}
+                landscape={true}
+            >
+                <div className="App">
+                    <Welcome/>
+                    <Info/>
+                </div>
+            </PDFExport>
+            <button className={'pdfButton'} onClick={() => pdfExport.current.save()}>generate pdf</button>
+        </Fragment>
+    );
 }
 
 export default App;
