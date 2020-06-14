a392f33849c822a9f33094c6a4e85807e045a0f0
Maxim Fishman <maxim.fishman@kcl.ac.uk>
2020-06-13 18:01:22 -0700

    remove pdf export

--- src/App/App.js
+++ src/App/App.js
@@ -1,30 +1,15 @@
-import React, {Fragment, useRef} from 'react';
+import React from 'react';
 import './App.css';
 import Welcome from "../Welcome/welcome";
 import Info from "../Info/info";
-import PDFExport from "@progress/kendo-react-pdf/dist/npm/PDFExport";
 
 
 function App() {
-    const pdfExport = useRef(null);
-
     return (
-        <Fragment>
-            <PDFExport
-                scale={0.75}
-                paperSize="A4"
-                margin="0cm"
-                fileName="CV-Maxim Fishman"
-                ref={pdfExport}
-                landscape={true}
-            >
-                <div className="App">
-                    <Welcome/>
-                    <Info/>
-                </div>
-            </PDFExport>
-            <button className={'pdfButton'} onClick={() => pdfExport.current.save()}>generate pdf</button>
-        </Fragment>
+        <div className="App">
+            <Welcome/>
+            <Info/>
+        </div>
     );
 }
 
